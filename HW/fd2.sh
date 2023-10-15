4a919e404900507c2265e3f7d15e58d98b1a1c9a
test2 <test2@ubuntu2.myguest.virtualbox.org>
2023-10-16 00:28:47 +0300

    correct

--- HW/fd2.sh
+++ HW/fd2.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 echo "This is a standart output that will be silenced" > /dev/null     
-echo "This is an error message that will be silenced"  2>&1 > /dev/null
-exit 0
+echo "This is an error message that will be silenced" > /dev/null 2>&1
