d23a72c91ab39ba4b7e8c06d73f391033785c6ee
test2 <test2@ubuntu2.myguest.virtualbox.org>
2023-10-18 23:31:25 +0300

    triangle#1

--- HW/triangle.sh
+++ HW/triangle.sh
@@ -2,9 +2,9 @@
 x=$1
 y=$2
 z=$3
-if [ "$x" -ge 1 ] && [ "$x" -le 100 ];then
-   if [ "$x" -eq "$y" ] && [ "$x" -eq "$z" ];then echo "EQUILATERAL"
-   elif [ "$x" -eq "$y" ] || [ "$x" -eq "$z" ] || [ "$y" -eq "$z" ];then echo "ISOSCELES"
+if [ $x -ge 1 ] && [ $x -le 100 ] && [ $y -ge 1 ] && [ $y -le 100 ] && [ $z -ge 1 ] && [ $z -le 100 ];then
+   if [ $x -eq $y ] && [ $x -eq $z ];then echo "EQUILATERAL"
+   elif [ $x -eq $y ] || [ $x -eq $z ] || [ $y -eq $z ];then echo "ISOSCELES"
    else echo "SCALENE"
    fi
 else 
