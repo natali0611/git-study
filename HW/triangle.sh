e2961bfbcbd8a3cb60fc3e2d9f4232de498f229e
test2 <test2@ubuntu2.myguest.virtualbox.org>
2023-10-19 22:59:09 +0300

    .

--- HW/triangle.sh
+++ HW/triangle.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ $1 -ge 1 ] && [ $1 -le 100 ] && [ $2 -ge 1 ] && [ $2 -le 100 ] && [ $3 -ge 1 ] && [ $3 -le 100 ];then
+if [[ $1 -ge 1 ]] && [[ $1 -le 100 ]] && [[ $2 -ge 1 ]] && [[ $2 -le 100 ]] && [[ $3 -ge 1 ]] && [[ $3 -le 100 ]];then
    if [ $1 -eq $2 ] && [ $1 -eq $3 ];then echo "EQUILATERAL"
    elif [ $1 -eq $2 ] || [ $1 -eq $3 ] || [ $2 -eq $3 ];then echo "ISOSCELES"
    else echo "SCALENE"
