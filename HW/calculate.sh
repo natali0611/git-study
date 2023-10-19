8f40252d0cc9c05d68b360e263f350ebd38f390c
test2 <test2@ubuntu2.myguest.virtualbox.org>
2023-10-19 23:38:48 +0300

    .

--- HW/calculate.sh
+++ HW/calculate.sh
@@ -2,10 +2,13 @@
 declare -i x y
 x=$1
 y=$2
-echo -e "Sum: $((x+y))\nProduct: $((x*y))" >> stdout
-if [[ $y -eq 0 ]]; then
-   echo "can't divide by 0"
-else
-echo "Quotient: $((x/y))" >> stdout
+if [ -n  "$1" ] && [ -n "$2" ]; then
+    echo -e "Sum: $((x+y))\nProduct: $((x*y))"
+    if [[ $y -eq 0 ]]; then
+    echo "can't divide by 0"
+    else
+    echo "Quotient: $((x/y))" 
+    fi
+    echo "Difference: $((x-y))"
+else echo "error"
 fi
-echo "Difference: $((x-y))" >> stdout
