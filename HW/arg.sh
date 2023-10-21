d0a5a400b3d817841ee75a1d5d5a2e155908baad
test2 <test2@ubuntu2.myguest.virtualbox.org>
2023-10-21 22:32:39 +0300

    .

--- HW/arg.sh
+++ HW/arg.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-size=$(stat --format=%s "$1")
-if [[ "$size" -eq 1024 ]] || [[ "$size" -lt 1024 ]];then
-echo "ok"
+if [ -n "$1" ];then
+    if [ $(wc -c "$1" | awk '{print $1}') -le 1024 ];then
+    echo "ok"
+    else
+    echo "fail"
+    fi
 else
-echo "fail"
+echo "input file"
 fi
-
