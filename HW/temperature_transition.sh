3b0d67c5e1c5e01fa0051187310ff3a5f9345081
test2 <test2@ubuntu2.myguest.virtualbox.org>
2023-10-24 22:20:39 +0300

    .

---
+++ HW/temperature_transition.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+gradus=${@:1}
+let kol=${#gradus}-1
+s=${gradus:0:$kol} 
+f=${gradus:(-1):1}
+ if [ "$f" = "C" ] && [ "$s" -ge "0" ];then
+ let sK=$s+273 
+    echo  ${sK}K
+ elif [ "$f" = "K" ] && [ "$s" -ge "273" ];then
+ let sC=$s-273
+     echo ${sC}C
+else
+ echo "error"
+ fi
