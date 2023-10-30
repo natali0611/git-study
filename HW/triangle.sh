8a05f74a89851cf4c5c6c5e3d9fc7cfc58d74102
test2 <test2@ubuntu2.myguest.virtualbox.org>
2023-10-30 20:25:44 +0300

    last

--- HW/triangle.sh
+++ HW/triangle.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-if [[ $1 -ge 1 ]] && [[ $1 -le 100 ]] && [[ $2 -ge 1 ]] && [[ $2 -le 100 ]] && [[ $3 -ge 1 ]] && [[ $3 -le 100 ]];then
-   if [ $1 -eq $2 ] && [ $1 -eq $3 ];then echo "EQUILATERAL"
-   elif [ $1 -eq $2 ] || [ $1 -eq $3 ] || [ $2 -eq $3 ];then echo "ISOSCELES"
-   else echo "SCALENE"
-   fi
-else 
-echo "incorrect input"
+#determines the type of a triangle based on its three sides
+
+args=("$@")
+
+if [ ${#args[@]} -ne 3 ]; then  # check count of args
+   echo "insert 3 argument, pls"
+   exit 1
 fi
+
+for a in "${args[@]}"; do     # check range of values
+   if ((a < 1 || a > 100)); then
+      echo "insert 3 arguments from 1 to 100"
+      exit 1
+   fi
+done
+
+if [ $1 -eq $2 ] && [ $1 -eq $3 ];then echo "EQUILATERAL"
+elif [ $1 -eq $2 ] || [ $1 -eq $3 ] || [ $2 -eq $3 ];then echo "ISOSCELES"
+else echo "SCALENE"
+fi
+
 
 
