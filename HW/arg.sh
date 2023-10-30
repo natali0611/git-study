a099abf99dad4560390a13a7536f88c7ea524907
test2 <test2@ubuntu2.myguest.virtualbox.org>
2023-10-30 20:38:14 +0300

    last

--- HW/arg.sh
+++ HW/arg.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-if [ -n "$1" ];then
-    if [ $(wc -c "$1" | awk '{print $1}') -le 1024 ];then
-    echo "ok"
-    else
-    echo "fail"
-    fi
-else
+#checks the size of a given file
+
+if [ "$1" == 0 ];then
 echo "input file"
+exit 1
 fi
+
+if [ $(wc -c "$1" | awk '{print $1}') -le 1024 ];then
+    echo "ok" 
+else
+    echo "fail"
+fi
+
