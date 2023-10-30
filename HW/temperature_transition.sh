a8d545b2facb0960841338caa28871e72f92e18f
test2 <test2@ubuntu2.myguest.virtualbox.org>
2023-10-30 20:47:43 +0300

    last

--- HW/temperature_transition.sh
+++ HW/temperature_transition.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+#convert temperatures between Celsius and Kelvin scales
+
 gradus=${@:1}
 let kol=${#gradus}-1
-s=${gradus:0:$kol} 
+
+#separate number from letter  
+s=${gradus:0:$kol}  
 f=${gradus:(-1):1}
- if [ "$f" = "C" ] && [ "$s" -ge "0" ];then
- let sK=$s+273 
-    echo  ${sK}K
- elif [ "$f" = "K" ] && [ "$s" -ge "273" ];then
- let sC=$s-273
-     echo ${sC}C
+
+if [ "$f" = "C" ] && [ "$s" -ge "0" ];then
+   let sK=$s+273 
+   echo  ${sK}K
+elif [ "$f" = "K" ] && [ "$s" -ge "273" ];then
+   let sC=$s-273
+   echo ${sC}C
 else
- echo "error"
- fi
+   echo "error"
+fi
