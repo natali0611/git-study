a9c09fc2334fcf8400622e3f1282c7d3a870e82c
test2 <test2@ubuntu2.myguest.virtualbox.org>
2023-10-30 19:54:27 +0300

    last

--- HW/listScript.sh
+++ HW/listScript.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-if [[ $1 -le 32 ]]; then
-alfa=("abcdefghijklmnopqrstuvwxyz")
-alf=${alfa:0:$1}
-for (( i=0; i<${#alf}; i++ )); do
-mkdir dir_${alf:$i:1}
-done
-else
+# generate count of directories with naming them alphabetically
+ 
+if [[ $1 -ge 32 ]]; then
 echo "Error!input number 1-32"
+exit 1
 fi
+
+alfa=({a..z})
+for (( i=0; i<$1; i++ )); do
+mkdir dir_${alfa[i]}
+done
+
